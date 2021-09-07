21270e995c723c9349163e38b2effc154ab32405
buptwch2018211702 <1357527606@qq.com>
2021-09-07 10:07:24 +0800

    feat: add excel read.

---
+++ managementplatformv1 3/managementplatformv1/uploadfile8001/src/main/java/com/earthquake/managementPlatform/service/ExcelFile.java
@@ -0,0 +1,12 @@
+package com.earthquake.managementPlatform.service;
+
+public class ExcelFile extends DisasterFile {
+    public ExcelFile() {
+        this.fileBehavior = new ExcelBehavior();
+    }
+
+    public ExcelFile(String filePath) {
+        this.fileBehavior = new ExcelBehavior();
+        this.filePath = filePath;
+    }
+}
